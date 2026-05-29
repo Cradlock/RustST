060717351e6e48d9d6364dab24e2229b1c283d96
cradlock <pefig8@gmail.com>
2026-05-29 21:00:30 +0600

    first code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s = 12;
+
+    let a = if s > 10 {
+        "greater than 10"
+    } else {
+        "less than or equal to 10"
+    };
+
+    println!("The value of a is: {}", a);
 }
